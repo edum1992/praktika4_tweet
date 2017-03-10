72af78d0faf486c047a764bca5f8539907e0d28b
Edu <edum1992@gmail.com>
2017-03-10 11:22:38 +0100

    rt

---
+++ praktika4_Tweet/src/praktika4_Tweet/Nagusia.java
@@ -0,0 +1,12 @@
+package praktika4_Tweet;
+
+import java.io.FileReader;
+
+public class Nagusia {
+	
+	public static void main(String[] args) {
+		FileReader fi = new FileReader(args[0]);
+		
+	}
+
+}
