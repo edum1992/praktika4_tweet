dae6aab0b16e3f3e711fb75efee974dbcbea880f
iker <zarragaemerando@gmail.com>
2017-03-10 17:56:00 +0100

    arff sortu

---
+++ praktika4_Tweet/src/praktika4_Tweet/Aurreprozesatzailea.java
@@ -0,0 +1,79 @@
+package praktika4_Tweet;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Aurreprozesatzailea {
+	
+	public static void main(String[] args) throws Exception {
+		FileReader fi = new FileReader(args[0]);
+		BufferedReader br = new BufferedReader(fi);
+		
+		
+		FileWriter fw = new FileWriter(args[1]);
+		BufferedWriter bw = new BufferedWriter(fw);
+		Aurreprozesatzailea.arffIdatzi(bw, Aurreprozesatzailea.datuakIrakurri(br));
+	}
+
+	
+	private static ArrayList<String[]> datuakIrakurri(BufferedReader br) throws Exception {
+		ArrayList<String[]> arraya = new ArrayList<>();
+		String s;
+		br.readLine();
+		while ((s = br.readLine()) != null) {
+			arraya.add(s.toLowerCase().trim().split("\",\""));
+		}
+		br.close();
+		for (int i = 0; i < arraya.size(); i++) {
+			String[] lista = arraya.get(i);
+			for (int j = 0; j < lista.length; j++) {
+				lista[j] = lista[j].replaceAll("[^A-Za-z0-9 ]", " ");
+			}
+			arraya.set(i, lista);
+		}
+		return arraya;
+	}
+		
+	
+	private static void arffIdatzi(BufferedWriter bw, ArrayList<String[]> array) throws IOException {
+		bw.write("@relation tweet");
+		bw.newLine();
+		bw.write("@attribute 'Topic' String");
+		bw.newLine();
+		bw.write("@attribute 'Sentiment' { positive, neutral, irrelevant, negative}");
+		bw.newLine();
+		bw.write("@attribute 'TweetId' numeric");
+		bw.newLine();
+		bw.write("@attribute 'TweetDate' String");
+		bw.newLine();
+		bw.write("@attribute 'TweetText' String");
+		bw.newLine();
+		bw.write("@DATA");
+		bw.newLine();
+		Iterator<String[]> i = array.iterator();
+		while (i.hasNext()) {
+			String[] oraingoa = i.next();
+			while (oraingoa.length < 5 && i.hasNext())
+				oraingoa = i.next();
+			bw.write("'" + oraingoa[0].replace("\"", "") + "',");
+			bw.write(oraingoa[1] + ",");
+			bw.write(oraingoa[2] + ",");
+			bw.write("'" + oraingoa[3] + "',");
+			bw.write("'" + oraingoa[4].trim().replace(",", "").replace("'", ""));
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+
+	}
+		
+	
+
+
+}
